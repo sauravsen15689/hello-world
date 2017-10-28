f3bdbc08288ff219d1a3dbf434ab2a1d04f40493
SAURAV SEN <sauravsen_15689@aitpune.edu.in>
2017-10-28 15:41:39 +0530

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+# define ll long long
+//int arr[1005] ;
+int main()
+{
+
+	ios_base::sync_with_stdio(false);cin.tie(NULL);
+     cout<<"hello world";
+return 0;
+
+}
